77cddefbaa7ba2ecf7eebe0ef4154818147c6abc
JohnUser <JohnUser@PJo6Futur>
2016-06-01 00:04:01 +0200

    - simplify code by using functions

--- Exercices/Opencv/rotation_cb.py
+++ Exercices/Opencv/rotation_cb.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import math
+import detect_lines
 from cmath import sqrt, phase
 from itertools import groupby
 from collections import Counter
@@ -11,20 +12,9 @@ def rotation_cb(img):
 
 # Load an color image in grayscale
 
-    height, width = img.shape[:2]
-    blank_image = np.zeros((height,width,3), np.uint8)
-    blank_image2 = np.zeros((height,width,3), np.uint8)
-    vecteur_image=np.zeros((height,width,3), np.uint8)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    edges = cv2.Canny(gray,150,150,apertureSize = 3)
-    minLineLength = 10
-    maxLineGap = 100
-    lines = cv2.HoughLinesP(edges,1,np.pi/180,90,minLineLength,maxLineGap)
-    i=0
-    for x1,y1,x2,y2 in lines[0]:
-        i=i+1
-        cv2.line(blank_image,(x1,y1),(x2,y2),(255,255,255),1)
     
+    height, width = img.shape[:2]
+    lines=detect_lines.detect_lines(img)    
     coord=[]
     for x1,y1,x2,y2 in lines[0]:
         norme=abs(sqrt((x1-x2)^2+(y1-y2)^2))
