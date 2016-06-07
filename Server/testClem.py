f7dc6d84d78360768c8a3af97e93c6cd5d1c7bf1
Sebseb <sebseb.lailler@gmail.com>
2016-06-08 00:18:26 +0200

    Intéraction serveur + requetes
    -ajout requetes 
    -mise dans la base de données

--- Server/testClem.py
+++ Server/testClem.py
@@ -1,52 +1,48 @@
 #/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+#Gros problème sur le NA8
 #WARNING LES BUGS AU NIVEAU DES ENVIRONNEMENTS VIRTUELS VIENNENT PTET DE LA PREMIERE LIGNE /usr/bin/env python
+#ne pas oublier le IGNORE
+
 from flask import Flask, request
 from flaskext.mysql import MySQL
-import requete 
+import reqTest 
+import datetime
 
 mysql = MySQL()
 app = Flask(__name__)
-
-app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_USER'] = 'seb1'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'azop'
-app.config['MYSQL_DATABASE_DB'] = 'EmpData'
+app.config['MYSQL_DATABASE_DB'] = 'bddproject'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
+mysql.init_app(app)  
 
-
-@app.route("/Authenticate/")
-def Authenticate():
-	
-    username = request.args.get('UserName')
-    password = request.args.get('Password')
-    
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO User (userId,userName,password) VALUES ('','John', 'John')")
-    cursor.execute("INSERT INTO User (userId,userName,password) VALUES ('','Jean', 'Jean')")
-    cursor.execute("INSERT INTO User (userId,userName,password) VALUES ('','Maria', 'Maria')")
-    cursor.execute("SELECT * from User where Username='" + username + "' and Password='" + password + "'")
-    conn.commit()
-    data = cursor.fetchone()
-    if data is None:
-    	return "Username or Password is wrong"
-    else:
-    	return "Logged in successfully"
-    	
-    
-     
      
 @app.route("/")
 def hello():
 	return "It's working !!!!!!!!!!!!!!!!!!!!!!!!!!!"
     
-@app.route("/detecte/")
+@app.route("/RemplissageFrigo/")
 def detecte():
-    codeBarres = requete.detection("5449000000996")
-    return codeBarres[0]+codeBarres[1]+codeBarres[2]
- 	
+	i=7
+	now = datetime.datetime.now()
+	dateToday = now.strftime("%Y-%m-%d")
+	
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	codeBarres = ["5449000000996","5449000014535","3071500419958","3073780591591","3564700014646","3023260005100","3270190021179","3033490968120","3496080210137","3095752126013","3596710061969"]
+	
+	
+	while( i<len(codeBarres)):
+		info = reqTest.detection(codeBarres[i])	
+		cursor.execute("INSERT INTO Elem_connu  (id,code_barres,nom,description,image_adress) VALUES (NULL,'" +codeBarres[i]+ "','" +info[0]+ "',NULL,NULL )")
+		cursor.execute("INSERT INTO Elem_frigo (id,code_barres,date_peremp,date_entree) VALUES (NULL,'" + codeBarres[i] +"',NULL,'"+dateToday+"')")
+		i += 1
+		
+	conn.commit()
+	return "ok"
+	
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
 
