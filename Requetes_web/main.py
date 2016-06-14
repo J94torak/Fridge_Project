cdd1d7b40526f4f1ed2f1d76d563adb9cd6a3a72
Clemence <Clemence@192.168.181.108>
2016-06-14 10:59:40 +0200

    Test classe Produit du fichier requete -> return 0 si code barre invalide

--- Requetes_web/main.py
+++ Requetes_web/main.py
@@ -2,24 +2,26 @@
 # -*- coding: utf-8 -*-
 import Recette
 import Produit
+import Requete
 
-
-produit1 = Produit.Produit("20056124")
-produit2 = Produit.Produit("3254560045217")
+produit1 = Requete.Produit("20056124")
+produit2 = Requete.Produit("3254560045217")
   
-produit1.detection()
-nom = produit1.nom
-description = produit1.description
-calories = produit1.calories
+valide = produit2.detection()
+if valide ==1:
+    nom = produit2.nom
+    description = produit2.description
+    calories = produit2.calories
 
-print nom
-print description
-print calories  
+    print nom
+    print description
+    print calories 
+ 
        
 liste =["tomate","salade","concombre","citron","thon","basilic frais","mais"]
 plat ="entree"
 
-recettes = Recette.Recettes(liste,plat)
+recettes = Requete.Recettes(liste,plat)
 recettes.choix()
 recettes.aff_choix()
 choix_recette ="Salade d'été d'Elvire"
@@ -29,4 +31,3 @@ recettes.aff_ingredients()
 
 recettes.do_pourcentage()
 recettes.aff_pourcentage()
-
